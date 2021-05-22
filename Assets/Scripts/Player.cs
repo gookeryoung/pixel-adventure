22a859c143a955a5ceb5619625a572bd55811734
Young <gooker_young@163.com>
2021-05-22 22:00:15 +0800

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 5.0f;
+    private Rigidbody2D _rigidBody2D;
+    private Animator _animator;
+
+    private float x;
+    private float y;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        if (x > 0)
+        {
+            _rigidBody2D.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            _animator.SetBool("run", true);
+        }
+
+        if (x < 0)
+        {
+            _rigidBody2D.transform.eulerAngles = new Vector3(0f, 180f, 0f);
+            _animator.SetBool("run", true);
+        }
+
+        if (x < 0.0001f && x > -0.0001f)
+        {
+            _animator.SetBool("run", false);
+        }
+
+        Run();
+    }
+
+    private void Run()
+    {
+        Vector3 movement = new Vector3(x, y, 0);
+
+        _rigidBody2D.transform.position += movement * speed * Time.deltaTime;
     }
 }
