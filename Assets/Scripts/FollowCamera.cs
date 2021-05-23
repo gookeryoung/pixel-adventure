ea9366e7ffa7f2ee3a34ad7dca8617704b39a431
Young <gooker_young@163.com>
2021-05-23 17:12:15 +0800

    update camera follow

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform player;
+    public float ahead = 3f;//X轴最大变量
+    public float speed = 1f;//速度
+    public float smooth = 2.5f;//平滑度
+    public float minX = 0f;
+    public float maxX = 53f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+        if (player.position.x > minX && player.position.x < maxX)
+        {
+            if (Mathf.Abs(transform.position.x - player.position.x) > ahead)
+            {
+                if (transform.position.x > player.position.x)
+                {
+                    targetPos = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+                }
+                else
+                {
+                    targetPos = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+                }
+            }
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * smooth);
+        // Debug.Log(player.position.x);
+    }
+}
